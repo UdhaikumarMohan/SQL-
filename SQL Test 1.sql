11e5b443cb8021d001d276d5399993372644b741
Udhaikumar Mohan <udhaikumar4@gmail.com>
2023-03-10 13:08:49 +0530

    Third commit

--- SQL Test 1.sql
+++ SQL Test 1.sql
@@ -112,26 +112,39 @@ join dbo.[Order Details] as D on O.OrderID = D.OrderID) as Ord_value group by Cu
 
 -- 12. Find the supplier who supplies the highest number of orders.
 
-create view vwSupplier as 
-(select S.SupplierID, S.ContactName, P.ProductID from dbo.Suppliers as S join dbo.Products as P on S.SupplierID = P.SupplierID);
-Go
+select top 1 SupplierID, CompanyName, COUNT(OrderID) as Supp_Ord from
+(select S.SupplierID, S.CompanyName, P.ProductID, O.OrderID from dbo.Suppliers as S
+join dbo.Products as P on S.SupplierID = P.SupplierID 
+join dbo.[Order Details]as O on P.ProductID = O.ProductID)
+as Ord_Count group by SupplierID, CompanyName order by Supp_Ord desc;
 
-select Top 1 with Ties SupplierID, count(*) as [Number of Supplies] from vwSupplier group by SupplierID order by [Number of Supplies] Desc;
+Declare @i int = 1
+select SupplierID, CompanyName, Ord_by_Supplier from 
+(select SupplierID, CompanyName, Count(SupplierID) as Ord_by_Supplier, dense_rank() over (Order by Count(SupplierID) desc) as Rank from
+(select S.SupplierID, S.CompanyName, P.ProductID, O.OrderID from dbo.Suppliers as S
+join dbo.Products as P on S.SupplierID = P.SupplierID 
+join dbo.[Order Details]as O on P.ProductID = O.ProductID)
+as Ord_Count group by SupplierID, CompanyName) as Top_Company where Rank = @i;
 
 -- 13. Which category has highest number of products.
 
---[select C.CategoryID, C.CategoryName, P.ProductID, Count(*) from dbo.Products as P join dbo.Categories as C on P.CategoryID = C.CategoryID group by C.CategoryID Order by count(*) desc;]
+select CategoryID, CategoryName from dbo.Categories where CategoryID = 
+(select top 1 CategoryID from
+(select CategoryID, COUNT(ProductID) as [Count of Products] from dbo.Products group by CategoryID) as Top_Category order by [Count of Products] desc);
 
-create view vwcategory as 
-(select C.CategoryID, C.CategoryName, P.ProductID from dbo.Products as P join dbo.Categories as C on P.CategoryID = C.CategoryID);
-Go
+select CategoryID, CategoryName as Top_Category from dbo.Categories where CategoryID = 
+(select CategoryID from
+(select CategoryID, COUNT(ProductID) as [Count of Products], DENSE_RANK() over (order by Count(ProductID) desc) 
+as Rank from dbo.Products group by CategoryID) as First_Category where Rank = 1);
 
-select CategoryName, COUNT(*) as [Number of Products] from vwcategory group by CategoryName order by [Number of Products] desc;
+select CategoryID, CategoryName, Count_of_Prod from
+(select C.CategoryID, C.CategoryName, count(P.ProductID) as Count_of_Prod, DENSE_RANK() over (order by count(P.ProductID) desc) as Rank
+from dbo.Categories as C 
+join dbo.Products as P on C.CategoryID = P.CategoryID
+group by C.CategoryID, CategoryName) as Top_Category where Rank = 1;
 
-
-select CategoryID, COUNT(*) as [Number of Products] from dbo.Products group by CategoryID order by [Number of Products] desc;
-
--- [select CategoryName from dbo.Categories where CategoryID = (select CategoryID, COUNT(*) as [Number of Products] from dbo.Products group by CategoryID);]
-
+select Top 1 CategoryID, CategoryName, Count_of_Prod from
+(select C.CategoryID, C.CategoryName, count(P.ProductID) as Count_of_Prod from dbo.Categories as C join dbo.Products as P on C.CategoryID = P.CategoryID
+group by C.CategoryID, CategoryName) as Top_Category order by Count_of_Prod desc;
 
 -- 14. 
