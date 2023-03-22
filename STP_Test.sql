6d4745ee5146e54634e9401bb959133ca9d1317b
Udhaikumar Mohan <udhaikumar4@gmail.com>
2023-03-22 19:55:23 +0530

    'STP_test_updated'

--- STP_Test.sql
+++ STP_Test.sql
@@ -11,6 +11,7 @@ Address varchar(50) not null, Constraint My_PK_1 Primary Key(Customer_ID));
 -- Room type and its Rent PerDay.
 
 Create table Room_Category(Category varchar(5) not null, Description text, Rent numeric (10,2),
+Tax_in_Percentage numeric(10,2), Total_Rent numeric(10,2),
 Constraint My_PK_2 Primary Key(Category));
 
 -- Room Numbers and its availability
@@ -40,7 +41,7 @@ references Room_Allotment(Booking_ID, Customer_ID, Room_Number));
 
 Create table Room_Rent(Booking_ID bigint not null,
 Customer_ID bigint not null,
-Room_Number varchar(50) not null, Rent_of_Stay numeric(10,2),
+Room_Number varchar(50) not null, Duration_in_Days int, Rent_of_Stay numeric(10,2),
 Constraint My_FK_5 Foreign Key (Booking_ID, Customer_ID, Room_Number) 
 references Room_Allotment(Booking_ID, Customer_ID, Room_Number));
 
@@ -56,33 +57,26 @@ Booking_ID bigint not null, Customer_ID bigint not null, Total_Bill_Amount numer
 
 -- STP for Customer Entry
 
-Create Procedure STP_Customer_Entry(@Customer_ID bigint, @Name varchar(50), @Phone varchar(50), @Address varchar(50))
+Alter Procedure STP_Customer_Entry(@Name varchar(50), @Phone varchar(50), @Address varchar(50))
 as
 begin
-	if exists(select Customer_ID from Customer_Details where Customer_ID = @Customer_ID)
-	begin
-		raiserror('Customer_ID already exists...', 16, 1)
-	end
-
-	else
-	begin
-		insert into Customer_Details values(@Customer_ID, @Name, @Phone, @Address)
-	end
+	Declare @Customer_ID bigint
+	set @Customer_ID = ((select MAX(Customer_ID) from Customer_Details) + 1)
+	insert into Customer_Details values(@Customer_ID, @Name, @Phone, @Address)
 end
 
-exec STP_Customer_Entry 1, 'Udhai', '952496', 'Woraiyur, Trichy'
-
-
-select * from Customer_Details
 
 -- STP for room types and its rent per day
 
 -- Create table Room_Category(Category varchar(5) not null, Description text, Rent numeric (10,2), Constraint My_PK_2 Primary Key(Category))
 
 
-Create Procedure STP_Room_Category_Insert(@Category varchar(5), @Description text, @Rent numeric(10,2))
+Alter Procedure STP_Room_Category_Insert(@Category varchar(5), @Description text, @Rent numeric(10,2), @Tax numeric(10,2))
 as
 begin
+	declare @Total_Rent numeric
+	set @Total_Rent = ((@Rent/100) * @Tax) + @Rent
+
 	if exists(select Category from Room_Category where Category = @Category)
 	begin
 		raiserror('Room category already exists...', 16, 1)
@@ -90,7 +84,7 @@ begin
 
 	else
 	begin
-		insert into Room_Category values(@Category, @Description, @Rent)
+		insert into Room_Category values(@Category, @Description, @Rent, @Tax, @Total_Rent)
 	end
 end
 
@@ -144,12 +138,17 @@ Room_Number varchar(50) not null Constraint My_FK_3 Foreign Key references Room_
 Reason text, Check_IN datetime,
 Constraint My_PK_4 Primary Key(Booking_ID, Customer_ID, Room_Number));*/
 
-Alter Procedure STP_Check_IN (@Customer_ID bigint, @Room_Number varchar(50), @Reason text)
+
+/*Create table Room_Rent(Booking_ID bigint not null,
+Customer_ID bigint not null,
+Room_Number varchar(50) not null, Duration_in_Days int, Rent_of_Stay numeric(10,2),
+Constraint My_FK_5 Foreign Key (Booking_ID, Customer_ID, Room_Number) 
+references Room_Allotment(Booking_ID, Customer_ID, Room_Number));*/
+
+Alter Procedure STP_Check_IN (@Booking_ID bigint, @Customer_ID bigint, @Room_Number varchar(50), @Reason text)
 as
 begin
-	declare @Booking_ID bigint
-	set @Booking_ID = ((select COUNT(Booking_ID) from Room_Allotment) + 1)
-
+	
 	/* We cannot call date function in the execution. So we declare Check_In date here in STP.
 	If we want to give date and time manually then we wil give input through execution. */
 
@@ -174,6 +173,18 @@ begin
 				insert into Room_Allotment values(@Booking_ID, @Customer_ID, @Room_Number, @Reason, @Check_IN)
 				
 				update Room_Availability set Availability = 'No' where Room_Number = @Room_Number
+
+				declare @Check_OUT datetime
+				declare @Duration int
+				declare @Rent numeric(10,2)
+
+				set @Check_OUT = Null
+				set @Duration = Null
+				set @Rent = Null
+
+				insert into Time_of_Stay values(@Booking_ID, @Customer_ID, @Room_Number, @Check_OUT, @Duration)
+
+				insert into Room_Rent values(@Booking_ID, @Customer_ID, @Room_Number, @Duration, @Rent)
 			end
 		end
 
@@ -188,6 +199,65 @@ begin
 		raiserror('Customer_ID does not exists. Please add Customer in Customer_Details...', 16, 1)
 	end
 end
+
+-- Check_OUT process
+
+/*Create table Time_of_Stay(Booking_ID bigint not null,
+Customer_ID bigint not null,
+Room_Number varchar(50) not null, Check_Out datetime, Duration int,
+Constraint My_FK_4 Foreign Key (Booking_ID, Customer_ID, Room_Number) 
+references Room_Allotment(Booking_ID, Customer_ID, Room_Number));*/
+
+Alter Procedure STP_Check_OUT(@Booking_ID bigint, @Room_Number varchar(50))
+as
+begin
+	
+	if ((select Room_Number from Room_Allotment where Booking_ID = @Booking_ID and Room_Number = @Room_Number) = @Room_Number)
+	begin
+		
+		if ((select Availability from Room_Availability where Room_Number = @Room_Number) = 'No')
+		begin
+			declare @Check_IN datetime
+			set @Check_IN = (select Check_IN from Room_Allotment where Booking_ID = @Booking_ID and Room_Number = @Room_Number)
+
+			declare @Check_OUT datetime
+			set @Check_OUT = GETDATE()
+
+			declare @Duration int
+			set @Duration = DATEDIFF(dd, @Check_IN, @Check_OUT) + 1
+
+			declare @Rent_of_Stay numeric(10,2)
+			set @Rent_of_Stay = (select Total_Rent from 
+			(select A.Room_Number, C.Category, C.Total_Rent 
+			from Room_Availability as A 
+			join Room_Category as C on A.Category = C.Category)
+			as Room_Rent where Room_Number = @Room_Number)
+
+			update Time_of_Stay set Check_Out = @Check_OUT, Duration = @Duration where Booking_ID = @Booking_ID and Room_Number = @Room_Number
+
+			update Room_Rent set Duration_in_Days = @Duration, Rent_of_Stay = @Rent_of_Stay
+
+			update Room_Availability set Availability = 'Yes' where Room_Number = @Room_Number
+	
+		end
+
+		else
+		begin
+			raiserror('This Room_Number is already checked out...', 16, 1)
+		end
+	end
+	
+	else
+	begin
+		raiserror('This Room is not booked under this Booking_ID, Plese provide correct Room_Number or Booking_ID...', 16,1)
+	end 
+end
+
+
+
+
+
+
 
 
 
--- STP_Test_Execution.sql
+++ STP_Test_Execution.sql
@@ -1,16 +1,29 @@
+-- Insert Customer_Details
 
+exec STP_Customer_Entry 'Udhai', '952496', 'Woraiyur, Trichy'
+
+exec STP_Customer_Entry 'Ramesh', '999382', 'Thuvarankuruchi, Trichy'
+
+exec STP_Customer_Entry 'Muthuram', '889781', 'Adayar, Chennai'
+
+exec STP_Customer_Entry 'Sada Sivan', '720102', 'Munichalai, Madurai'
+
+
+select * from Customer_Details
 
 -- Insert Room Number
 
-exec STP_Room_Category_Insert @Category = 'O+', @Description = 'Premier Room', @Rent = 5000.00
+exec STP_Room_Category_Insert @Category = 'O+', @Description = 'Premier Room', @Rent = 5000.00, @Tax = 10
 
-exec STP_Room_Category_Insert @Category = 'A+', @Description = 'Double Bed with View', @Rent = 4000.00
+exec STP_Room_Category_Insert @Category = 'A+', @Description = 'Double Bed with View', @Rent = 4000.00, @Tax = 5
 
-exec STP_Room_Category_Insert @Category = 'A', @Description = 'Double Bed without View', @Rent = 3500.00
+exec STP_Room_Category_Insert @Category = 'A', @Description = 'Double Bed without View', @Rent = 3500.00, @Tax = 5
 
-exec STP_Room_Category_Insert @Category = 'B+', @Description = 'Standard room with Double Bed', @Rent = 2500.00
+exec STP_Room_Category_Insert @Category = 'B+', @Description = 'Standard room with Double Bed', @Rent = 2500.00, @Tax = 2.50
 
-exec STP_Room_Category_Insert @Category = 'B', @Description = 'Standard Room with Single Bed', @Rent = 2000.00
+exec STP_Room_Category_Insert @Category = 'B', @Description = 'Standard Room with Single Bed', @Rent = 2000.00, @Tax = 2.50
+
+Select * from Room_Category
 
 -- Execute update room number
 
@@ -64,13 +77,36 @@ select * from Room_Availability
 
 -- Executing Check_IN
 
-exec STP_Check_IN @Customer_ID = 1, @Room_Number = 'P1', @Reason = 'Bussiness Meeting'
+exec STP_Check_IN @Booking_ID = 1, @Customer_ID = 1, @Room_Number = 'P1', @Reason = 'Bussiness Meeting'
 
-exec STP_Check_IN @Customer_ID = 1, @Room_Number = 'P2', @Reason = 'Bussiness Meeting'
+exec STP_Check_IN @Booking_ID = 1, @Customer_ID = 1, @Room_Number = 'P2', @Reason = 'Bussiness Meeting'
+
+exec STP_Check_IN @Booking_ID = 2, @Customer_ID = 2, @Room_Number = 'S1', @Reason = 'Transit'
+
+exec STP_Check_IN @Booking_ID = 3, @Customer_ID = 4, @Room_Number = 'T3', @Reason = 'Tour'
+
+exec STP_Check_IN @Booking_ID = 3, @Customer_ID = 4, @Room_Number = 'T2', @Reason = 'Tour'
+
+exec STP_Check_IN @Booking_ID = 4, @Customer_ID = 3, @Room_Number = 'F1', @Reason = 'Conference'
+
+
+exec STP_Check_OUT @Booking_ID = 3, @Room_Number = 'T2'
+
+exec STP_Check_OUT @Booking_ID = 1, @Room_Number = 'P1'
+
+exec STP_Check_OUT @Booking_ID = 1, @Room_Number = 'P2'
+
+
+
 
 select * from Room_Availability
 
 select * from Room_Allotment 
+
+select * from Time_of_Stay
+
+select * from Room_Rent
+
 
 
 
